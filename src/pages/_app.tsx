535300c1027d021e8be6b11c19acfd43607253e7
runejac <69790920+runejac@users.noreply.github.com>
2023-12-08 14:48:33 +0100

    added new speedinsights from Vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import PrimaryLayout from "../components/layouts/primary/PrimaryLayout";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.scss";
 import { polyfill } from "seamless-scroll-polyfill";
 import { useEffect } from "react";
@@ -17,6 +18,7 @@ export default function App({
     <PrimaryLayout>
       <Component {...pageProps} />
       <Analytics />
+      <SpeedInsights />
     </PrimaryLayout>
   );
 }
