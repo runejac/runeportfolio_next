0e379f1b377e6f48ac64720625e188020b2bfbff
runejac <69790920+runejac@users.noreply.github.com>
2023-12-12 17:25:57 +0100

    rollback

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.scss";
 import { polyfill } from "seamless-scroll-polyfill";
 import React, { useEffect } from "react";
-import Script from "next/script";
 
 export default function App({
   Component,
@@ -16,11 +15,6 @@ export default function App({
 
   return (
     <PrimaryLayout>
-      <Script
-        async={true}
-        src={"9e3daab4-9492-41da-8572-c942a55cc23d"}
-        data-website-id={"https://eu.umami.is/script.js"}
-      />
       <Component {...pageProps} />
       <SpeedInsights />
     </PrimaryLayout>
