215a1dc15cd647fe50eec8c32fdf7c3c92578a15
runejac <69790920+runejac@users.noreply.github.com>
2023-12-12 18:00:30 +0100

    GA?

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import "../styles/globals.scss";
 import { polyfill } from "seamless-scroll-polyfill";
 import React, { useEffect } from "react";
+import Script from "next/script";
+
+const GTM_ID = "GTM-TWBGSRVG";
 
 export default function App({
   Component,
@@ -15,6 +18,14 @@ export default function App({
 
   return (
     <PrimaryLayout>
+      <Script
+        id={"google-tag-manager"}
+        strategy="afterInteractive"
+      >{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}')`}</Script>
       <Component {...pageProps} />
       <SpeedInsights />
     </PrimaryLayout>
