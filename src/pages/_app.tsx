5fa023f0d7f0970dcbf415ac591988d5bed83f32
runejac <69790920+runejac@users.noreply.github.com>
2023-12-15 00:08:20 +0100

    added NEXT_PUBLIC to the var

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  const DATA_WEBSITE_ID = process.env.DATA_WEBSITE_ID;
+  const DATA_WEBSITE_ID = process.env.NEXT_PUBLIC_DATA_WEBSITE_ID;
   useEffect(() => {
     polyfill();
   }, []);
@@ -18,7 +18,7 @@ export default function App({
   return (
     <PrimaryLayout>
       <Script
-        src="https://umami-seven-sigma.vercel.app/script.js"
+        src={"https://umami-seven-sigma.vercel.app/script.js"}
         data-website-id={DATA_WEBSITE_ID}
         strategy="afterInteractive"
       />
